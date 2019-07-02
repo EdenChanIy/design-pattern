e6ccdf89a5b641cbd4ee201b6f2793bcd0910cc9
EdenChanIy <chenjh246@mail2.sysu.edu.cn>
2019-07-02 15:20:42 +0800

    整合鸭子行为

--- src/Duck.java
+++ src/Duck.java
@@ -3,5 +3,16 @@
  * @Date: 2019/7/2 11:08
  */
 public class Duck {
+    QuackBehavior quackBehavior;
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+    public void swim(){
+        //游泳方法
+    }
 
+    public void display()
+    {
+        //外观
+    }
 }
