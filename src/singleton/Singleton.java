c4a69d56bda0e3fd0cca337e773704a5cad3f647
EdenChanIy <29560008+EdenChanIy@users.noreply.github.com>
2019-07-11 12:25:00 +0800

    单例模式

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package singleton;
+
+/**
+ * @Author: EdenChanIy
+ * @Date: 2019/7/11 11:47
+ * 经典的单例模式实现
+ */
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(uniqueInstance==null){
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
