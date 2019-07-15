f45eda13abdbb7bbd379b6ab91693b3c982985b6
EdenChanIy <29560008+EdenChanIy@users.noreply.github.com>
2019-07-15 16:21:17 +0800

    命令模式

---
+++ src/command/LightOffCommand.java
@@ -0,0 +1,17 @@
+package command;
+
+/**
+ * @Author: EdenChanIy
+ * @Date: 2019/7/15 16:16
+ */
+public class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light){
+        this.light = light;
+    }
+
+    public void execute(){
+        light.off();
+    }
+}
---
+++ src/command/NoCommand.java
@@ -0,0 +1,10 @@
+package command;
+
+/**
+ * @Author: EdenChanIy
+ * @Date: 2019/7/15 16:19
+ * 空对象
+ */
+public class NoCommand implements Command {
+    public void execute(){}
+}
